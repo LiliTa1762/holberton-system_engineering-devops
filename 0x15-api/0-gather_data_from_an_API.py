398d689c46e4afc155a4b30e16a86e8c382c1abd
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-02 03:02:22 +0000

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(sys.argv[1]))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1]))
+
+    data_user = user.json()
+    data_todos = todos.json()
+
+    all_list = []
+    count_task = 0
+    count_all_task = 0
+
+    for t in data_todos:
+        if t["completed"] is True:  # access to the value of the key completed
+            count_task += 1
+            all_list.append(t["title"])
+            # add the value that is in the key Title in position t
+        count_all_task += 1
+    print("Employee {} is done with tasks({}/{})"
+          .format(data_user['name'], count_task, count_all_task))
+
+    for x in all_list:
+        print(("\t{}").format(x))
