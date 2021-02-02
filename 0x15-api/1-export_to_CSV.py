22eb7295575b460eb855054d3d59e6f0530de9e2
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-02 03:02:47 +0000

    Export to CSV file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(sys.argv[1]))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1]))
+
+    data_user = user.json()
+    data_todos = todos.json()
+
+with open(sys.argv[1] + '.csv', mode='w') as user_file:
+    user_writer = csv.writer(user_file, delimiter=',', quoting=csv.QUOTE_ALL)
+
+    for t in data_todos:
+        user_writer.writerow([t["userId"], data_user["username"],
+                              t["completed"], t["title"]])
