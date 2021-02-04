9b64877ad940021e03e90531c361e7b92a7848e5
Lilita1762 <lilibeth.tabares@gmail.com>
2021-02-04 22:26:11 +0000

    First request to Reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Function that queries the Reddit API and
+returns the number of subscribers (not active users,
+total subscribers) for a given subreddit.
+If an invalid subreddit is given,
+the function should return 0.
+"""
+
 
 import requests
 import json
